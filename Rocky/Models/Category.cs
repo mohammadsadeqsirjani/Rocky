4d2769ca60a0c29f15a6414cdad49545181853ed
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-05 17:26:05 +0330

    Update Category.cs

--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -1,14 +1,13 @@
-﻿using System;
+﻿using Rocky.Models.Common;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace Rocky.Models
 {
-    public class Category
+    public class Category : BaseEntity
     {
-        [Key]
-        public int Id { get; set; }
-
         [Required]
         public string Name { get; set; }
 
@@ -16,5 +15,7 @@ namespace Rocky.Models
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Display Order for category must be greater than 0")]
         public int DisplayOrder { get; set; }
+
+        public List<Product> Products { get; set; }
     }
 }
