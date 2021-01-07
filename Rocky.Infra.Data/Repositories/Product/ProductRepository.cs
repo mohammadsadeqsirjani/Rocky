032b6c5e70cfa36d69c8bc22bf76a154c5f23a57
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-07 23:00:46 +0330

    Create ProductRepository.cs
    add sync product repository service

---
+++ Rocky.Infra.Data/Repositories/Product/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Rocky.Domain.Interfaces.Product;
+using Rocky.Infra.Data.Persistence;
+using Rocky.Infra.Data.Repositories.Common;
+
+namespace Rocky.Infra.Data.Repositories.Product
+{
+    public class ProductRepository : Repository<Domain.Entities.Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
