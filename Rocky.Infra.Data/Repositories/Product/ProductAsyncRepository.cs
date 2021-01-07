ccea083e97759881b5832d7af25fdaec73c24b84
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-07 23:00:32 +0330

    Create ProductAsyncRepository.cs
    add product async repository service

---
+++ Rocky.Infra.Data/Repositories/Product/ProductAsyncRepository.cs
@@ -0,0 +1,13 @@
+﻿using Rocky.Domain.Interfaces.Product;
+using Rocky.Infra.Data.Persistence;
+using Rocky.Infra.Data.Repositories.Common;
+
+namespace Rocky.Infra.Data.Repositories.Product
+{
+    public class ProductAsyncRepository : AsyncRepository<Domain.Entities.Product>, IProductAsyncRepository
+    {
+        public ProductAsyncRepository(ApplicationDbContext db) : base(db)
+        {
+        }
+    }
+}
