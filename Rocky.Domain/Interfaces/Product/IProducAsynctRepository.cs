b3dc2adcce906fd10f222b923b731a073fccdf36
Mohammad Sadeq Sirjani <45625225+mohammadsadeqsirjani@users.noreply.github.com>
2021-01-07 22:59:54 +0330

    Create IProducAsynctRepository.cs
    add product async repository

---
+++ Rocky.Domain/Interfaces/Product/IProducAsynctRepository.cs
@@ -0,0 +1,8 @@
+﻿using Rocky.Domain.Interfaces.Common;
+
+namespace Rocky.Domain.Interfaces.Product
+{
+    public interface IProductAsyncRepository : IAsyncRepository<Entities.Product>
+    {
+    }
+}
